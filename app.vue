d80f445531ce1af7c53acfac1bd8947ccb9c344c
NicolayBelyaev <nickb2147@gmail.com>
2024-05-31 23:06:40 +0300

    custom button v1

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import CustomButton from "~/features/customButton.vue";
+const data = () => {
+  return {
+    isDark: true
+  }
+}
+</script>
+
+<template>
+  <div>
+    <custom-button style-class="selector" :is-dark='isDark'>Click!</custom-button>
+    <custom-button style-class="cart" :is-dark=true>Click!</custom-button>
+  </div>
+</template>
---
+++ features/customButton.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+  const props = defineProps({
+    clickHandler: Function,
+    styleClass: String,
+    isDark: Boolean
+  })
+  const style = computed(() => {
+    return `${props.styleClass + " " + props.styleClass + "__"}${props.isDark ? 'dark' : 'light'}`
+  })
+</script>
+
+<template>
+  <button @click="clickHandler" :class=style>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+  .selector {
+    height: 35px;
+    font-size: 13px;
+    padding-left: 15px;
+    padding-right: 15px;
+    border-radius: 20px;
+
+    &__light {
+      color: #ffffff;
+      background-color: rgb(215, 82, 0);
+    }
+    &__dark {
+      color: #000000;
+      background-color: #009b95;
+    }
+  }
+  .cart {
+    border-radius: 50px;
+    width: 80px;
+    height: 80px;
+    cursor: pointer;
+
+    &__light {
+      color: #000000;
+      background-color: rgba(255, 255, 255, .8);
+    }
+
+    &__dark {
+      color: #8a8a8a;
+      background-color: rgba(0, 0, 0, 0.8);
+    }
+  }
+</style>
